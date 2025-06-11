1c5ee294bec3d02f554cbb8b1a245643158b9a4f
Josemiles-ctr <josephotai209@gmail.com>
2025-06-11 17:40:30 +0300

    Modified the users migartion by placing role after email just for better view in the database without scrolling

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('user_type')->default('customer');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('user_type')->default('customer');
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
