2ef3c91a9b93964c2acb5c29105379bfb45a1c68
Josemiles-ctr <josephotai209@gmail.com>
2025-06-16 19:07:55 +0300

    Ebling only venders to be fetched for the customer to chart with

---
+++ .history/app/Livewire/Chat_20250616181321.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Chat extends Component
+{
+    public$
+    public function render()
+    {
+        return view('livewire.chat');
+    }
+}
