80f50138c3907542b7a8fbe9eb7a7395947246f1
Josemiles-ctr <josephotai209@gmail.com>
2025-06-23 13:54:30 +0300

    Adding livewire to control modal rendering instead of alpine js

---
+++ .history/resources/views/livewire/manufacturer-rawmaterial-order.blade_20250623114458.php
@@ -0,0 +1,23 @@
+<div x-data="{ open: false }" class="relative">
+
+    <!-- Layout Header -->
+    <x-layouts.dashboard-component-heading
+      title="Manufacturer Inventory"
+      :description="__('Manage your inventory of products and Raw Materials')">
+    </x-layouts.dashboard-component-heading>
+  
+    <!-- Trigger Button -->
+    <div class="p-6 flex justify-end">
+      <button @click="open = 'openorder'"
+              class="bg-blue-600 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-700 transition">
+        Order Rawmaterials
+      </button>
+    </div>
+   
+  
+  
+  </div>
+  
+  
+  </div>
+  
