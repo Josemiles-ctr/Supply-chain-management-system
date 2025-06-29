84291157b67f4658d6e0488b31827dbeb7cd008c
Josemiles-ctr <josephotai209@gmail.com>
2025-06-29 12:01:59 +0300

    Making purchase order creation only possible to manufacturers

---
+++ .history/app/Filament/Resources/RawMaterialsPurchaseOrderResource/Pages/CreateRawMaterialsPurchaseOrder_20250629100017.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\Resources\RawMaterialsPurchaseOrderResource\Pages;
+
+use Filament\Actions;
+use Illuminate\Support\Facades\Auth;
+use Filament\Resources\Pages\CreateRecord;
+use App\Filament\Resources\RawMaterialsPurchaseOrderResource;
+
+class CreateRawMaterialsPurchaseOrder extends CreateRecord
+{
+    protected static string $resource = RawMaterialsPurchaseOrderResource::class;
+    
+    protected function mutateFormDataBeforeCreate(array $data): array
+{
+    $data['created_by'] = Auth::id();
+    $dat
+    return $data;
+}
+}
---
+++ .history/app/Filament/Resources/RawMaterialsPurchaseOrderResource_20250628174313.php
@@ -0,0 +1,210 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use App\Models\User;
+use Filament\Tables;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use App\Models\RawMaterial;
+use Filament\Resources\Resource;
+use Filament\Tables\Actions\Action;
+use Illuminate\Support\Facades\Auth;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Notifications\Notification;
+use App\Models\RawMaterialsPurchaseOrder;
+use Filament\Forms\Components\DatePicker;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\RawMaterialsPurchaseOrderResource\Pages;
+use App\Filament\Resources\RawMaterialsPurchaseOrderResource\RelationManagers;
+use App\Filament\Resources\RawMaterialsPurchaseOrderResource\Pages\EditRawMaterialsPurchaseOrder;
+use App\Filament\Resources\RawMaterialsPurchaseOrderResource\Pages\ViewRawMaterialsPurchaseOrder;
+use App\Filament\Resources\RawMaterialsPurchaseOrderResource\Pages\ListRawMaterialsPurchaseOrders;
+use App\Filament\Resources\RawMaterialsPurchaseOrderResource\Pages\CreateRawMaterialsPurchaseOrder;
+
+
+class RawMaterialsPurchaseOrderResource extends Resource
+{
+    protected static ?string $model = RawMaterialsPurchaseOrder::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    public static function canViewAny(): bool
+{
+    return Auth::user()?->role == 'manufacturer' || Auth::user()?->role == 'supplier';
+}
+public static function shouldRegisterNavigation(): bool
+{
+    return Auth::user()?->role === 'manufacturer' || Auth::user()?->role === 'supplier';
+}
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                
+                Select::make('rawMaterial_id')
+                ->label('Raw Material')
+                ->options(RawMaterial::all()->pluck('name', 'id'))
+                ->required()
+                ->searchable()
+                ->preload()
+                ->native(false),
+                Select::make('supplier_id')
+                    ->label('Supplier')
+                    ->options(User::query()->where('role', 'supplier')->pluck('name', 'id'))
+                    ->required()
+                    ->searchable()
+                    ->preload()
+                    ->native(false),
+                TextInput::make('quantity')
+                    ->required()
+                    ->numeric(),
+                TextInput::make('price_per_unit')
+                    ->required()
+                    ->numeric(),
+                DatePicker::make('order_date')
+                    ->required(),
+                DatePicker::make('expected_delivery_date')
+                    ->required(),
+                Select::make('status')
+                    ->options(RawMaterialsPurchaseOrder::STATUSES)
+                    ->default('pending')
+                    -
+                    ->required(),
+                TextInput::make('notes')
+                    ->maxLength(255)
+                    ->default(null),
+                TextInput::make('delivery_option')
+                    ->required(),
+                TextInput::make('created_by')
+                    ->required()
+                    ->numeric(),
+                TextInput::make('total_price')
+                    ->numeric()
+                    ->default(null),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('rawMaterial.name')
+                    ->label('Raw Material')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('supplier.name')
+                    ->sortable()
+                    ->visible(fn ($record) => Auth::user()?->role == 'manufacturer'),
+                Tables\Columns\TextColumn::make('quantity')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('price_per_unit')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('total_price')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('order_date')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('expected_delivery_date')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('status')
+                    ->label('Status')
+                    ->sortable()
+                    ->color(fn (string $state): string => match ($state) {
+                        'pending' => 'warning',
+                        'confirmed' => 'success',
+                        'cancelled' => 'danger',
+                        'delivered' => 'success',
+                        default => 'gray',
+                    }),
+                Tables\Columns\TextColumn::make('delivery_option'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Action::make('markAsDelivered')
+                ->label('Mark As Delivered')
+                ->icon('heroicon-o-truck')
+                ->color('success')
+                ->action(function ($record) {
+                    $record->update(['status' => 'delivered']);
+                    Notification::make()
+                    ->title('Order marked as delivered')
+                    ->success()
+                    ->send();
+                })
+                ->visible(fn ($record) => $record->status == 'confirmed' && Auth::User()->role=='manufacturer'),
+                Action::make('CancelOrder')
+                ->label('Cancel Order')
+                ->icon('heroicon-o-x-mark')
+                ->color('danger')
+                ->action(function ($record) {
+                    $record->update(['status' => 'cancelled']);
+                    Notification::make()
+                    ->title('Order cancelled')
+                    ->success()
+                    ->send();
+                })
+                ->visible(fn ($record) => $record->status == 'pending' && Auth::User()->role == 'manufacturer' || $record->status == 'confirmed' && Auth::User()->role == 'manufacturer'),
+                Action::make('ConfirmOrder')
+                ->label('Confirm Order')
+                ->icon('heroicon-o-check')
+                ->color('success')
+                ->action(function ($record) {
+                    $record->update(['status' => 'confirmed']);
+                    Notification::make()
+                    ->title('Order confirmed')
+                    ->success()
+                    ->send();
+                })
+                ->visible(fn ($record) => $record->status == 'pending' && Auth::User()->role == 'supplier'),
+                ViewAction::make(),
+                EditAction::make()
+                ->visible(fn ($record) => Auth::user()?->role === 'manufacturer'),
+                
+            ])
+            ->bulkActions([
+                BulkActionGroup::make([
+                DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListRawMaterialsPurchaseOrders::route('/'),
+            'create' => Pages\CreateRawMaterialsPurchaseOrder::route('/create'),
+            'view' => Pages\ViewRawMaterialsPurchaseOrder::route('/{record}'),
+            'edit' => Pages\EditRawMaterialsPurchaseOrder::route('/{record}/edit'),
+        ];
+    }
+}
